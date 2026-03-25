609cc7a50bbb0a006f017388371a75bacea9ccbc
Jxz887 <jazz0656871156@gmail.com>
2026-03-25 11:34:01 +0700

    Coding crs/index create images

---
+++ src/assets/images.js
@@ -0,0 +1,16 @@
+export const heroImg =
+  "https://images.unsplash.com/photo-1622286342621-4bd786c2447c?auto=format&fit=crop&w=1600&q=80";
+
+export const team1 =
+  "https://images.unsplash.com/photo-1517832606299-7ae9b720a186?auto=format&fit=crop&w=800&q=80";
+export const team2 =
+  "https://images.unsplash.com/photo-1621605815971-fbc98d665033?auto=format&fit=crop&w=800&q=80";
+export const team3 =
+  "https://images.unsplash.com/photo-1512690459411-b0fd1c86b8ce?auto=format&fit=crop&w=800&q=80";
+
+export const cut1 =
+  "https://images.unsplash.com/photo-1517837016564-bfc2a7ff8f0d?auto=format&fit=crop&w=800&q=80";
+export const cut2 =
+  "https://images.unsplash.com/photo-1622288432450-277d0fef5ed6?auto=format&fit=crop&w=800&q=80";
+export const cut3 =
+  "https://images.unsplash.com/photo-1585747860715-2ba37e788b70?auto=format&fit=crop&w=800&q=80";
